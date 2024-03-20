16e0c741e10c967bfe749c62c5768b0f23bcef18
Nasia Davies <nasia.davies@education.nsw.gov.au>
2024-03-20 17:14:12 +1100

    Authentication

--- eduprod/views.py
+++ eduprod/views.py
@@ -1,15 +1,11 @@
+
 from django.core import serializers
 from django.shortcuts import render
 from .models import Question
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    # questions = Question.objects.all()
-    questions = Question.objects.order_by('?')[:5]
+    questions = Question.objects.all()
     questions_json = serializers.serialize('json', questions)
-   
-    categories = Question.objects.values_list('category', flat=True).distinct()
-    
-    return render(request, 'eduprod/index.html', {
-        'questions_json': questions_json,
-        'categories': categories
-        })
+    return render(request, 'eduprod/index.html', {'questions_json': questions_json})
